48f6229af4b7f7bad22dd8e22b677c1a2f6310e4
M CPE <mmmiko@hotmail.com>
2022-05-09 18:27:43 +0700

    html + form + POST + allow everry host

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import pymongo
 import os
-import Model
+from static import Model
 
 app = Flask(__name__)
 link = "mongodb+srv://"+os.environ['dbuser']+":"+os.environ['dbpass']+"@cluster0.zltoe.mongodb.net/homeStorage?retryWrites=true&w=majority"
@@ -13,9 +13,17 @@ print(fridge)
 m = Model.Food('m', 2)
 print(m.name)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route('/', methods=['GET'])
+def index():
+    print(request)
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def search():
+    print(request)
+    if request.form['name'] != '':
+        print(request.form['name'])
+    return render_template('index.html')
 
 @app.route('/get', methods=['GET'])
 def hello_world2():
@@ -23,8 +31,10 @@ def hello_world2():
 
 @app.route('/post', methods=['POST'])
 def add():
-    fridge.insert_one({"name": m.name, "amount": m.amount})
+    x = fridge.insert_one({"name": m.name, "amount": m.amount})
+    print(x.acknowledged)
+    print(request)
     return '200'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
