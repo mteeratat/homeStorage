9b900945cecc77b7312d91de95263e29510062a0
M CPE <mmmiko@hotmail.com>
2022-05-10 14:46:46 +0700

    can search + can add to database + index,add,base,search html

--- home.py
+++ home.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pymongo
 import os
-from static import Model
 
 app = Flask(__name__)
 link = "mongodb+srv://"+os.environ['dbuser']+":"+os.environ['dbpass']+"@cluster0.zltoe.mongodb.net/homeStorage?retryWrites=true&w=majority"
@@ -9,32 +8,40 @@ client = pymongo.MongoClient(link)
 
 homeStorage = client.homeStorage
 fridge = homeStorage.fridge
-print(fridge)
-m = Model.Food('m', 2)
-print(m.name)
 
 @app.route('/', methods=['GET'])
 def index():
-    print(request)
     return render_template('index.html')
 
-@app.route('/', methods=['POST'])
+@app.route('/search', methods=['GET'])
 def search():
-    print(request)
-    if request.form['name'] != '':
-        print(request.form['name'])
-    return render_template('index.html')
+    response = fridge.find({'name': request.args.get('name')})
+    res = [r['name'] for r in response]
+    return render_template('search.html', name=res)
 
-@app.route('/get', methods=['GET'])
-def hello_world2():
-    return 'Hello World2'
-
-@app.route('/post', methods=['POST'])
+@app.route('/add', methods=['GET','POST'])
 def add():
-    x = fridge.insert_one({"name": m.name, "amount": m.amount})
-    print(x.acknowledged)
-    print(request)
-    return '200'
+    res = ""
+    if request.method == 'POST':
+        print('x')
+        x = fridge.insert_one({'name': request.form['name'], 'amount': request.form['num']})
+        # print(x.acknowledged)
+        if x.acknowledged:
+            res = "Add complete"
+        else: 
+            res = "Incomplete"
+    return render_template('add.html', res=res)
+
+# @app.route('/get', methods=['GET'])
+# def hello_world2():
+#     return 'Hello World2'
+
+# @app.route('/post', methods=['POST'])
+# def add():
+#     x = fridge.insert_one({"name": m.name, "amount": m.amount})
+#     print(x.acknowledged)
+#     print(request)
+#     return '200'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
